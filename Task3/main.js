76f28babc34842f944c6c679822fddf99a426c7c
Hawraa-a <hawra8516@gmail.com>
2025-12-25 08:59:32 +0400

    Task3

---
+++ Task3/main.js
@@ -0,0 +1,25 @@
+let email = "hawraa@gmail.com";
+let pass = "1234";
+let attempts = 3;
+
+function askLogin() {
+
+    while (attempts > 0) {
+        userEmail = prompt("Enter your email:");
+        userPassword = prompt("Enter your password:");
+        if (userEmail == email && userPassword == password) {
+            console.log("Login successful");
+            break;
+        }
+        attempts--;
+        console.log("Wrong!");
+    }
+    if (attempts == 0) {
+        console.log("Wait 30 s then you can try again");
+        setTimeout(() => {
+            attempts = 3;
+            askLogin();
+        }, 30000); 
+    }
+}
+askLogin();
