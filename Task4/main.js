fa921c3e59527e66bb0bf727b2c12e2f15f0142a
Hawraa-a <hawra8516@gmail.com>
2025-12-29 10:17:33 +0400

    Task 4

---
+++ Task4/main.js
@@ -0,0 +1,8 @@
+
+function sayHello() {
+  console.log("Hello");
+}
+
+btn.addEventListener("mouseover", sayHello);
+btn.addEventListener("click", sayHello);
+btn.addEventListener("mouseout", sayHello);
